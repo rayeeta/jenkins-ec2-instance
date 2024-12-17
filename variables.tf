5f866b7ac3b389eda5929acc94af073bfef0ee11
DevOps and Cloud Engineer Ray Eta. E. <115152999+rayeeta@users.noreply.github.com>
2024-12-17 08:48:43 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "key_name" {
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0866a3c8686eaeeba" # Example AMI ID for Ubuntu
+  default     = "ami-0e2c8caa4b6378d8c" # Example AMI ID for Ubuntu
 }
